78fa139eb85f416599a71c12a922c1cad69323aa
Melissa Vaiden <melissavaiden@gmail.com>
2022-02-08 15:02:07 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    makeToDo(document.querySelector('#new-task-description').value);
+  })
 });
+
+function makeToDo(toDo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deleteToDo);
+  btn.innerHTML = 'x'
+  p.textContent = `${toDo}`;
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function deleteToDo(e) {
+  e.target.parentNode.remove();
+}
